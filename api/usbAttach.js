26b93733df4d86ad1cb25aefd976bb7c9f79d992
Will <willbc49@gmail.com>
2019-06-11 17:26:14 +0100

    Add auto detach re attach

--- api/usbAttach.js
+++ api/usbAttach.js
@@ -23,59 +23,27 @@ async function attachUSB(data) {
   let vmObject = await gatherDetailsFromEditVM(data.server, data.id);
   let rawdata = fs.readFileSync("config/servers.json");
   let servers = JSON.parse(rawdata);
+  let attached = {};
 
   Object.keys(servers[data.server].vm.details).forEach(vmId => {
     let vm = servers[data.server].vm.details[vmId];
     if (vm.edit && vm.edit.pcis && vm.status === "started") {
       vm.edit.usbs.forEach(usbDevice => {
         if (usbDevice.id === data.usbId && vmId !== data.id && usbDevice.checked) {
-          console.log(usbDevice); //todo this is attached to another vm, need to detach
+          attached = {usbId: usbDevice.id, vmId, vm};
         }
       });
     }
   });
 
-  // { template_os: 'linux',
-  //   domain_persistent: '1',
-  //   domain_clock: 'utc',
-  //   domain_arch: 'x86_64',
-  //   domain_oldname: 'Hass.io',
-  //   domain_name: 'Hass.io',
-  //   domain_desc: '',
-  //   domain_ovmf: '1',
-  //   media_cdrom: '',
-  //   media_drivers: '',
-  //   gpu_bios: '',
-  //   nic_0_mac: '52:54:00:8a:f5:c5',
-  //   vcpus: [ '0' ],
-  //   disks: [ '/mnt/user/domains/HASS.io/hassos_ova-1.11.qcow2' ],
-  //   shares: [ { source: '', target: '' }, { source: '', target: '' } ],
-  //   usbs:
-  //    [ { id: '045e:02e6',
-  //        name: 'Microsoft Corp. Wireless XBox Controller Dongle' },
-  //      { id: '062a:3633', name: 'MosArtCorp.' },
-  //      { id: '0a5c:21ec', name: 'Broadcom Corp.' },
-  //      { id: '0b05:180a',
-  //        name:
-  //         'ASUSTek Computer Broadcom BCM20702 Single-Chip Bluetooth 4.0 + LE' },
-  //      { id: '1532:0203', name: 'Razer USA' },
-  //      { id: '1e71:170e', name: 'NZXT', checked: true } ],
-  //   pcis:
-  //    [ { id: '00:1a.0' },
-  //      { id: '00:1d.0' },
-  //      { id: '02:00.2' },
-  //      { id: '02:00.3' },
-  //      { gpu: true, checked: true, id: 'vnc' },
-  //      { gpu: true, id: '01:00.0' },
-  //      { gpu: true, id: '02:00.0' },
-  //      { sound: true, checked: true, id: '' },
-  //      { sound: true, id: '00:1b.0' },
-  //      { sound: true, id: '01:00.1' },
-  //      { sound: true, id: '02:00.1' } ] }
+  if (attached) {
+    await requestAttach(data.server, attached.vmId, attached.usbId, servers[data.server].authToken, attached.vm.edit, true);
+  }
+
   return requestAttach(data.server, data.id, data.usbId, servers[data.server].authToken, vmObject.edit);
 }
 
-async function requestAttach(ip, id, usb, auth, vmObject) {
+async function requestAttach(ip, id, usb, auth, vmObject, detach) {
   return axios({
     method: "POST",
     url: "http://" + ip + "/plugins/dynamix.vm.manager/templates/Custom.form.php",
@@ -84,7 +52,7 @@ async function requestAttach(ip, id, usb, auth, vmObject) {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
       "X-Requested-With": "XMLHttpRequest"
     },
-    data: await buildForm(ip, auth, usb, id, vmObject),
+    data: await buildForm(ip, auth, usb, id, vmObject, detach),
     httpAgent: new http.Agent({ keepAlive: true })
   }).then((response) => {
     return response.data;
@@ -93,7 +61,7 @@ async function requestAttach(ip, id, usb, auth, vmObject) {
   });
 }
 
-async function buildForm(ip, auth, usb, id, vmObject) {
+async function buildForm(ip, auth, usb, id, vmObject, detach) {
   let staticPart = "template%5Bos%5D=" + vmObject.template_os +
     "&domain%5Bpersistent%5D=" + vmObject.domain_persistent +
     "&domain%5Buuid%5D=" + id +
@@ -123,18 +91,17 @@ async function buildForm(ip, auth, usb, id, vmObject) {
     staticPart += "&shares%5B" + index + "%5D%5Btarget%5D=" + share.target;
   });
 
-  console.log(vmObject.pcis);
   vmObject.pcis.forEach(pciDevice => {
-    console.log(pciDevice);
     staticPart += "&pci%5B%5D=" + encodeURI(pciDevice.id) + (pciDevice.checked ? '' : '%23remove');
   });
-  console.log(staticPart);
 
   vmObject.usbs.forEach(usbDevice => {
-    staticPart += "&usb%5B%5D=" + encodeURI(usbDevice.id) + ((usbDevice.checked || usbDevice.id === usb) ? '' : '%23remove');
+    let condition = (usbDevice.checked || usbDevice.id === usb);
+    if (detach) {
+      condition = (usbDevice.checked && usbDevice.id !== usb);
+    }
+    staticPart += "&usb%5B%5D=" + encodeURI(usbDevice.id) + (condition ? '' : '%23remove');
   });
-
-  console.log(staticPart);
 
   return staticPart;
 }
