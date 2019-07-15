00ac5e3fcc9d7e4b9c8539b08a132ff9c97716a7
Will <willbc49@gmail.com>
2019-07-15 11:24:23 +0100

    Rearrange

--- app/node_modules/vuetify/src/components/VDatePicker/util/isDateAllowed.ts
+++
@@ -1,7 +0,0 @@
-export type AllowedDateFunction = (date: string) => boolean
-
-export default function isDateAllowed (date: string, min: string, max: string, allowedFn: AllowedDateFunction | undefined) {
-  return (!allowedFn || allowedFn(date)) &&
-    (!min || date >= min) &&
-    (!max || date <= max)
-}
