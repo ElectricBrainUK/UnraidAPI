40a79187de215c9e902f42521a9afbd9d14e2d2c
Will <willbc49@gmail.com>
2019-11-29 23:45:31 +0000

    Break down a bit more

--- utils/Unraid.js
+++ utils/Unraid.js
@@ -505,43 +505,47 @@ function extractPCIData(response) {
   return pcis;
 }
 
+function extractIndividualGPU(gpuInfo, gpuNo, vmObject, response) {
+  while (gpuInfo.includes("<option value='")) {
+    let row = extractValue(gpuInfo, "<option value='", ">");
+    let gpu = {};
+    gpu.gpu = true;
+    gpu.id = row.substring(0, row.indexOf("'"));
+    gpu.name = extractValue(extractValue(gpuInfo, "<option value='", "/option>"), ">", "<");
+    if (row.includes("selected")) {
+      gpu.checked = true;
+      gpu.position = gpuNo;
+      if (gpuNo > 0) {
+        vmObject.edit.pcis.forEach((device, index) => {
+          if (device.id === gpu.id) {
+            vmObject.edit.pcis.splice(index, 1);
+            vmObject.edit.pcis.push(gpu);
+          }
+        });
+      }
+    }
+
+    let gpuModel = extractValue(response.data, "<td>Graphics Card:</td>", "</table>");
+    if (gpuModel.includes("<td>VNC Video Driver:</td>")) {
+      gpu.model = extractReverseValue(extractValue(gpuModel, "<select name=\"gpu[" + gpuNo + "][model]\"", "selected>"), "'", "value='");
+      gpu.keymap = extractReverseValue(extractValue(gpuModel, "<select name=\"gpu[" + gpuNo + "][keymap]\"", "selected>"), "'", "value='");
+    }
+
+    gpu.bios = extractReverseValue(extractValue(response.data, "<td>Graphics ROM BIOS:</td>", " name=\"gpu["), "\"", "value=\"");
+
+    if (gpuNo === 0) {
+      vmObject.edit.pcis.push(gpu);
+    }
+
+    gpuInfo = gpuInfo.replace("<option value='", "");
+  }
+}
+
 function extractGPUData(response, vmObject) {
   let gpuNo = 0;
   while (response.data.includes("<td>Graphics Card:</td>")) {
     let gpuInfo = extractValue(response.data, "<td>Graphics Card:</td>", "</td>");
-    while (gpuInfo.includes("<option value='")) {
-      let row = extractValue(gpuInfo, "<option value='", ">");
-      let gpu = {};
-      gpu.gpu = true;
-      gpu.id = row.substring(0, row.indexOf("'"));
-      gpu.name = extractValue(extractValue(gpuInfo, "<option value='", "/option>"), ">", "<");
-      if (row.includes("selected")) {
-        gpu.checked = true;
-        gpu.position = gpuNo;
-        if (gpuNo > 0) {
-          vmObject.edit.pcis.forEach((device, index) => {
-            if (device.id === gpu.id) {
-              vmObject.edit.pcis.splice(index, 1);
-              vmObject.edit.pcis.push(gpu);
-            }
-          });
-        }
-      }
-
-      let gpuModel = extractValue(response.data, "<td>Graphics Card:</td>", "</table>");
-      if (gpuModel.includes("<td>VNC Video Driver:</td>")) {
-        gpu.model = extractReverseValue(extractValue(gpuModel, "<select name=\"gpu[" + gpuNo + "][model]\"", "selected>"), "'", "value='");
-        gpu.keymap = extractReverseValue(extractValue(gpuModel, "<select name=\"gpu[" + gpuNo + "][keymap]\"", "selected>"), "'", "value='");
-      }
-
-      gpu.bios = extractReverseValue(extractValue(response.data, "<td>Graphics ROM BIOS:</td>", " name=\"gpu["), "\"", "value=\"");
-
-      if (gpuNo === 0) {
-        vmObject.edit.pcis.push(gpu);
-      }
-
-      gpuInfo = gpuInfo.replace("<option value='", "");
-    }
+    extractIndividualGPU(gpuInfo, gpuNo, vmObject, response);
     gpuNo++;
     response.data = response.data.replace("<td>Graphics Card:</td>", "");
   }
