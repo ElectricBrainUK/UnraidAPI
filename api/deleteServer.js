d66e80e9e8268ca2de3c46dc36c0075fc04e1de1
Will <willbc49@gmail.com>
2020-05-20 11:19:40 +0100

    Ensure proper deletion

--- api/deleteServer.js
+++ api/deleteServer.js
@@ -7,17 +7,25 @@ export default function(req, res, next) {
   req.on("data", (chunk) => {
     body.push(chunk);
   }).on("end", async () => {
-    const rawdata = fs.readFileSync("config/servers.json");
-    let servers = JSON.parse(rawdata);
-
     const ip = Buffer.concat(body).toString();
 
-    servers[ip] = undefined;
-
-    fs.writeFileSync("config/servers.json", JSON.stringify(servers));
+    for (let i = 0; i < 5; i++) {
+      setTimeout(() => {
+        deleteIP(ip)
+      }, 5000 * i);
+    }
 
     response.status = 200;
     res.send(response);
   });
 };
+
+function deleteIP(ip) {
+  const rawdata = fs.readFileSync("config/servers.json");
+  let servers = JSON.parse(rawdata);
+
+  servers[ip] = undefined;
+
+  fs.writeFileSync("config/servers.json", JSON.stringify(servers));
+}
 
