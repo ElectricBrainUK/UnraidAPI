1d19c795485753183dc70c52b34731f93644f421
Aaron Osteraas <aaron.osteraas@gmail.com>
2021-03-20 22:34:02 +1100

    start update on get servers api rourte

--- pages/api/servers/index.ts
+++ pages/api/servers/index.ts
@@ -3,61 +3,58 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getUnraidDetails } from 'lib/getUnraidDetails';
 import { ServerMap } from 'models/server';
 import { scrapeHTML, scrapeMainHTML } from 'lib/scraper';
+import { parseServers } from 'lib/storage/servers';
+import { getKeyStorage } from 'lib/config';
+import { readMqttKeys } from 'lib/storage/secure';
 
 // function getDockerDetails (req: NextApiRequest, res: NextApiResponse) {
 //   const
 // }
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-  let servers: ServerMap = {};
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const keyStorage = getKeyStorage();
+  const authHeader = req.headers.authorization;
   try {
-    servers = JSON.parse(fs.readFileSync('config/servers.json').toString());
+    const servers = await parseServers();
+    if (
+      (!authHeader ||
+        Object.keys(authHeader).length < Object.keys(servers).length) &&
+      keyStorage !== 'config'
+    ) {
+      let response = {};
+      Object.keys(servers).forEach((ip) => {
+        response[ip] = true;
+      });
+      res.send({ servers: response });
+      return;
+    }
+    const response = {
+      servers,
+      status: 200,
+    };
   } catch (e) {
     console.log('Failed to retrieve config file, creating new.');
-    if (!fs.existsSync('config/')) {
-      fs.mkdirSync('config/');
-    }
-    fs.writeFileSync('config/servers.json', JSON.stringify(servers));
+    // if (!fs.existsSync("config/")){
+    //   fs.mkdirSync("config/");
+    // }
+    // fs.writeFileSync("config/servers.json", JSON.stringify(servers));
   }
-  // if (
-  //   (!req.headers.authorization ||
-  //     Object.keys(req.headers.authorization).length <
-  //       Object.keys(servers).length) &&
-  //   process.env.KeyStorage !== 'config'
-  // ) {
-  //   let response = {};
-  //   Object.keys(servers).forEach((ip) => {
-  //     response[ip] = true;
-  //   });
-  //   res.send({ servers: response });
-  //   return;
-  // }
+
   let response = {
-    servers,
-    status: 401,
-    details: [],
+    servers: {},
+    status: 200,
   };
 
-  if (
-    // process.env.KeyStorage === 'config' &&
-    !req.headers.authorization ||
-    req.headers.authorization.length <= 2
-  ) {
-    req.headers.authorization = fs
-      .readFileSync(
-        (process.env.KeyStorage ? process.env.KeyStorage + '/' : 'secure/') +
-          'mqttKeys',
-      )
-      .toString();
-  }
-  console.log(req.headers.authorization);
-  const details = getServerDetails(
-    response.servers,
-    JSON.parse(req.headers.authorization),
-  );
-  // getUnraidDetails(response.servers, JSON.parse(req.headers.authorization));
+  const loadAuth =
+    keyStorage === 'config' && (!authHeader || authHeader.length <= 2);
+  const auth = loadAuth ? await readMqttKeys() : authHeader;
+  // if (keyStorage === "config" && (!authHeader || authHeader.length <= 2)) {
+  //   const thing = await readMqttKeys()
+  //   authHeader = fs.readFileSync((keyStorage + "/" : "secure/") + "mqttKeys");
+  // }
+
+  getUnraidDetails(response.servers, JSON.parse(req.headers.authorization));
   response.status = 200;
-  response.details = details;
   res.send(response);
 }
 
