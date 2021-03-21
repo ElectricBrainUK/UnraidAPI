10614727dad5020d9001cd4c34b533d13c54b8ac
Aaron Osteraas <aaron.osteraas@gmail.com>
2021-03-21 14:43:25 +1100

    Get eslint working again

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,29 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
-    'plugin:vue/recommended'
+    // 'plugin:vue/recommended'
   ],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-  }
-}
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+  },
+};
