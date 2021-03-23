3293dd68d64f537ff9db9927e562ccecd44cf4f8
will <willbc49@gmail.com>
2021-03-23 15:24:27 +0000

    Rename function and force authToken to base64 format

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -7,14 +7,11 @@ export default async function (
   req: ApiBodyRequest<LoginBody>,
   res: NextApiResponse,
 ) {
-  const response = await connectToServer(req.body);
+  const response = await storeServerDetails(req.body);
   res.send(response);
 }
 
-// TODO this doesn't, strictly speaking, connect to any server, that happens
-// elsewhere. Should consider renaming function or changing how connections to
-// servers are handled.
-async function connectToServer({ ip, user, password }: LoginBody) {
+async function storeServerDetails({ ip, user, password }: LoginBody) {
   let response = { message: '' };
 
   try {
@@ -30,10 +27,11 @@ async function connectToServer({ ip, user, password }: LoginBody) {
     // TODO this has changed such that, if the server already exists from the
     // file that has been read in, it does not overwrite the entry. Is that
     // desired behaviour?
+    // TODO I think this just makes sure that an empty object exists instead of null but let me know if you think its redundant
     const serverExists = servers[ip] !== undefined;
     servers[ip] = serverExists ? servers[ip] : {};
 
-    const authToken = Buffer.from(`${user}:${password}`).toString();
+    const authToken = Buffer.from(`${user}:${password}`).toString("base64");
     keys[ip] = authToken;
 
     await Promise.all([writeMqttKeys(keys), writeServersJson(servers)]);
